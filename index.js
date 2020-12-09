c54f4befbb1e7a0c8d16c0f27e6cae277dc0392c
Samuel C Moore <samuel.c.moore1221@gmail.com>
2020-12-09 17:29:45 -0600

    adding application prep step

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-let DomainSearchField = document.getElementById('nameCheapDomainSearchField');
-let DomainSearchButton = document.getElementById('DomainSearchButton');
-let domainNameField = document.getElementById('domainNameField');
-let domainControlPanelButton = document.getElementById('domainControlPanelButton');
+const DomainSearchField = document.getElementById('nameCheapDomainSearchField');
+const DomainSearchButton = document.getElementById('DomainSearchButton');
+const domainNameField = document.getElementById('domainNameField');
+const domainControlPanelButton = document.getElementById('domainControlPanelButton');
 const domainCopyButton = document.getElementById('AddNewDomainCopyButton');
 const addnewDomainText = document.getElementById('AddNewDomainText');
+const findTargetResult = document.getElementById('findTargetResult');
+const findjarfilecmdText = document.getElementById('runJarFileCommand');
+const codsFileText = document.getElementById('codsFileText')
+
 let domain = "something.com";
 const sshkeycmd = "cat ~/.ssh/id_rsa.pub | pbcopy";
+const mavenPackagecmd = "./mvnw package";
+const findJarFileCmd = String.raw`find target -name \*.jar`
+let runJarFilecmd = "YOUR_JAR_FILE";
+let codsfileConts = `BUILD_COMMAND='./mvnw package'\nJAR_FILE=target/blog-0.0.1-SNAPSHOT.jar`
 
 
 
@@ -40,10 +48,20 @@ domainControlPanelButton.addEventListener("click", () =>{
     window.open(`https://ap.www.namecheap.com/domains/domaincontrolpanel/${domain}`)
 })
 
+findTargetResult.addEventListener('change', () =>{
+    runJarFilecmd = `java -jar ${findTargetResult.value}`
+    findjarfilecmdText.innerHTML = runJarFilecmd;
+    codsFileText.innerHTML = `BUILD_COMMAND='./mvnw package'\nJAR_FILE=${runJarFilecmd}`
+    clipboard('runJarFileCommandCopy', runJarFilecmd);
+})
+
 clipboard('nsOneCopy','ns1.digitalocean.com');
 clipboard('nsTwoCopy','ns2.digitalocean.com');
 clipboard('nsThreeCopy','ns3.digitalocean.com');
 clipboard('copySshCommandButton',sshkeycmd);
+clipboard('copyMavenPackageCommandButton',mavenPackagecmd);
+clipboard('findJarFileButton',findJarFileCmd);
+
 
 
 
